9d1318fcf57045fe777472a21651d65e14934da8
Shubham Choubey <shubhamchoubey96@gmail.com>
2024-12-15 09:32:53 +0530

    05 commit

--- src/Arrays/Target.java
+++ src/Arrays/Target.java
@@ -1,23 +1,24 @@
 package Arrays;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Target {
 
 
     public static void main(String[] args) {
-        int[] newArr = {1,3,7,9,2};
-        int target=25;
-        System.out.println(Arrays.toString(arrWithTarget(newArr,target)));
+        int[] newArr = {1, 3, 7, 9, 2};
+        int target = 25;
+        System.out.println(Arrays.toString(arrWithTargetHM(newArr, target)));
     }
 
     public static int[] arrWithTarget(int[] arr, int target) {
-        int[] temp =new int[2];
+        int[] temp = new int[2];
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i]+arr[j] == target) {
-                    temp = new int[]{arr[i ], arr[j]};
+                if (arr[i] + arr[j] == target) {
+                    temp = new int[]{arr[i], arr[j]};
 
                 }
             }
@@ -26,5 +27,18 @@ public class Target {
 
     }
 
+    //With hashmap
+    public static int[] arrWithTargetHM(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        System.out.println(map);
 
+        return arr;
+    }
+
+
+    //
 }
